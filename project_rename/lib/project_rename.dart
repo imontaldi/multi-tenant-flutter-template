cf42f4e71d2553ad8db4a0e46645bff59e9d9d15
Ignacio Montaldi <87707566+imontaldi@users.noreply.github.com>
2023-06-16 09:59:05 -0300

    mejora project rename

--- project_rename/lib/project_rename.dart
+++ project_rename/lib/project_rename.dart
@@ -1,12 +1,29 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:path/path.dart' as path;
+
 void projectRename() {
+  final rootProjectDir = Directory("..\\.");
+
+  final List<FileSystemEntity> rootProjectDirEntities =
+      rootProjectDir.listSync().toList();
+
+  late Directory targetDirectory;
+
+  for (var element in rootProjectDirEntities) {
+    if (element is Directory &&
+        !path.basename(element.path).contains(".") &&
+        !FileSystemEntity.identicalSync(element.path, Directory.current.path)) {
+      targetDirectory = Directory(element.path);
+    }
+  }
+
   final configFileContent =
-      File("../flutter_template/config.json").readAsStringSync();
+      File("${targetDirectory.path}/config.json").readAsStringSync();
   final configJson = jsonDecode(configFileContent) as Map<String, dynamic>;
 
-  File pubspec = File("../flutter_template/pubspec.yaml");
+  File pubspec = File("${targetDirectory.path}/pubspec.yaml");
 
   String originalPubspecContent = pubspec.readAsStringSync();
 
@@ -14,14 +31,15 @@ void projectRename() {
       originalPubspecContent.indexOf(":") + 2,
       originalPubspecContent.indexOf("\n"));
 
-  Directory projectDirectory = Directory("../$oldProjectName");
+  // print(targetDirectory.path);
+  // print(configJson['proyectName'].toString());
 
   try {
     //Se sobreescribe el nombre del proyecto en pubspec.yaml
     pubspec.writeAsStringSync(originalPubspecContent.replaceAll(
         oldProjectName, configJson['proyectName']));
-    //Se cambia el nombre del directorio
-    projectDirectory.renameSync("../${configJson["projectName"]}");
+    // Se cambia el nombre del directorio
+    targetDirectory.renameSync("${configJson["projectName"]}");
     print("Renombrado exitoso");
   } catch (e) {
     pubspec.writeAsStringSync(originalPubspecContent);
